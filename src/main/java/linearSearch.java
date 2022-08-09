fd5b348ee8a2ecc33415fec4a5ba2ce3b33dbf2b
GuilhermeKageyama <guiproc@gmail.com>
2022-08-09 11:18:49 -0300

    intial commit

--- src/main/java/linearSearch.java
+++ src/main/java/linearSearch.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class linearSearch {
+public class linearSearch {
+    private static int index;
+
+    public static int main(String[] args) {
+
+        public static int linearSeach(int[] list,int target) {
+            for(index = 0; index < list.length; index++){
+                if (list[index] == target){
+                    return index;
+                }
+
+            }
+            return -1;
+        }
+    }
 }
